77b6a98909eeadfd49e7c5bde323043966a0af87
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2021-07-23 03:04:23 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css"; 
 
 function App() {
-
-
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={call()}>Llamar</button>
+        <button onClick={()=>{call()}}>Llamar</button>
       </header>
     </div>
   );
-
 }
 
-function call(){
-  window.parent.postMessage({ action: 'LLAMAR', tel: '099460981'}, '*');
+function call() {
+  window.parent.postMessage({ action: "LLAMAR", tel: "099460981" }, "*");
 }
+/*
+function callWithFramework() {
+  var windowProxy;
+
+  windowProxy = new Porthole.WindowProxy(
+    window.location.protocol + "//" + window.location.hostname
+  );
+  windowProxy.addEventListener(onResponse);
+  windowProxy.post({
+    action: "callWithPhone",
+    destination: "3056776828",
+    campaign: "dev->",
+  });
+
+  function onResponse(messageEvent) {
+    var response = messageEvent.data.guid;
+  }
+}*/
 
 export default App;
