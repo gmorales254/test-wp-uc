f5fce51f0c704dce5428474c55e98ba7e0af1513
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2021-07-23 01:47:57 -0300

    Subida de archivos

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={call()}>Llamar</button>
+      </header>
+    </div>
+  );
+
+  function call(){
+    window.parent.postMessage({ action: 'LLAMAR', tel: '099460981'}, '*');
+    
+  }
+
+}
+
+export default App;
